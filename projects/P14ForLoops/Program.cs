7d9983bc654c63b4d97ed1fc14b4bec09cfea352
Petter Söderberg <petter.soderberg@student.forsbergsskola.se>
2022-02-02 17:29:55 +0100

    P14ForLoop assignment completed

---
+++ projects/P14ForLoops/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ForLoops
+{
+    class  Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Loop showing 1-1000.");
+            
+            for (int i = 1; i <= 1000; i++) 
+            {
+                Console.WriteLine(i);   
+            }
+            Console.WriteLine("\nLoop showing 200-100.");
+            
+            for (int i = 200; i >= 100; i--) 
+            {
+                Console.WriteLine(i);   
+            }
+            Console.WriteLine("\nLoop showing 1024, divided by 2, down to 1.");
+            
+            for (int i = 1024; i >= 1; i= i/2) 
+            {
+                Console.WriteLine(i);   
+            }
+        } 
+        
+    }
+}
+
