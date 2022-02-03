295b11ff132140ab9993a2dc44c77fd1a3fad3c0
Erick Vaghi <vaghi.erick@gmail.com>
2022-02-03 11:19:50 +0100

    P1MethodsErick Completed

--- projects/P1MethodsErick/Program.cs
+++ projects/P1MethodsErick/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Methods
+{
+    class Program
+    {
+        static void Main()
+        {
+            CountUP();
+            CountDown();
+            CountUP();
+            CountDown();
+        }
+        static void CountUP()
+        {
+            Console.WriteLine("\nCounting from 0 to 5");
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void CountDown()
+        {
+            Console.WriteLine("\nCounting from 5 to 0");
+            for(int i = 5; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
