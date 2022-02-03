f5a93709d2108be5bb42e090956bfb661a8be08b
Petter Söderberg <petter.soderberg@student.forsbergsskola.se>
2022-02-03 11:22:22 +0100

    P1MethodsPetter Finished

--- projects/P1MethodsPetter/Program.cs
+++ projects/P1MethodsPetter/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Methods
+{
+    class Program
+    {
+        static void Main()
+        {
+            CountUp();
+            CountDown();
+            CountUp();
+            CountDown();
+        }
+        static void CountUp()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void CountDown()
+        {
+            for (int i = 5; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+    
+    
+}
