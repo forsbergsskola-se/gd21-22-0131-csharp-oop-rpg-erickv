17deab62d15345940b41eea7d0d5fdbcd594f014
Erick Vaghi <vaghi.erick@gmail.com>
2022-02-03 17:51:22 +0100

    P3ReturnTyper completed (Erick)

--- projects/P3ReturnTypeErick/Program.cs
+++ projects/P3ReturnTypeErick/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ReturnType
+{
+    class Program
+    {
+        static int next = 0;
+        static int prev = 0;
+        static void Main()
+        {
+            
+            Console.Write("Which Fibonacci Number do you want? ");
+            int pos = Convert.ToInt32(Console.ReadLine());
+            double result = Fibonacci(pos);
+            Console.WriteLine(result);
+        }
+
+       static int Fibonacci(int position)
+        {
+            for (int i = 0; i < position; i++)
+            {
+                if (next == 0)
+                {
+                    next = 1;
+                }
+                else
+                {
+                    int temp = next;
+                    next = next + prev;
+                    prev = temp;
+                }
+            }
+            return next;
+        }
+    }
+}
