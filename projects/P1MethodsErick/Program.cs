4a2dc09508f67d555d4e6a4e8fc1684a3a7bb802
Erick Vaghi <vaghi.erick@gmail.com>
2022-02-03 12:19:17 +0100

    Assignment 2.2 Parameters Completed (Erick)

--- projects/P1MethodsErick/Program.cs
+++ projects/P1MethodsErick/Program.cs
@@ -5,29 +5,62 @@
 
         static void Main()
         {
-            for (int to = 1; to <= 3; to++)
+            /*for (int to = 1; to <= 3; to++)
             {
                 int from = to;
-                CountUP(to);
-                CountDown(from);
-            }
+                //CountUP(to);
+                //CountDown(to, from);
+            }*/
             
+            int from = 2;
+            int to = 3;
+            Count(from, to);
+            
+            from = 100;
+            to = 101;
+            Count(from, to);
+            
+            from = 10;
+            to = 8;
+            Count(from, to);
+            
+            from = 1;
+            to = -1;
+            Count(from, to);
+            
+            from = 1337;
+            to = 1337;
+            Count(from, to);
+
+
         }
-        static void CountUP(int num1)
+        
+       /* static void CountUP(int num1)
         {
             Console.WriteLine($"\nCounting from 0 to {num1}");
             for (int i = 0; i <= num1; i++)
             {
                 Console.WriteLine(i);
             }
-        }
+        }*/
 
-        static void CountDown(int num2)
+        static void Count(int a, int b)
         {
-            Console.WriteLine($"\nCounting from {num2} to 0");
-            for(int i = num2; i >= 0; i--)
+            if (a<b)
             {
-                Console.WriteLine(i);
+                Console.WriteLine($"\nCounting from {b} to {a}");
+                for(int i = a; i <= b; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"\nCounting from {a} to {b}");
+                for(int i = a; i >= b; i--)
+                {
+                    Console.WriteLine(i);
+                }
             }
         }
     }
