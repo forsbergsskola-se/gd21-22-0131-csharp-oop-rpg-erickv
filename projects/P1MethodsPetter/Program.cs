88ed0634f267f942c18b6dfb4c419f4dca23e363
Petter Söderberg <petter.soderberg@student.forsbergsskola.se>
2022-02-03 11:59:33 +0100

    P1MethodsPetter:  2.1 - Parameter Petter Finished

--- projects/P1MethodsPetter/Program.cs
+++ projects/P1MethodsPetter/Program.cs
@@ -4,22 +4,30 @@
  {
   static void Main()
   {
-   CountUp();
-   CountDown();
-   CountUp();
-   CountDown();
+   int to = 1;
+   CountUp(to);
+   int from = 1;
+   CountDown(from);
+   to = 2;
+   CountUp(to);
+   from = 2;
+   CountDown(from);
+   to = 3;
+   CountUp(to);
+   from = 3;
+   CountDown(from);
   }
 
-  static void CountUp()
+  static void CountUp(int a)
   {
-   for (int i = 0; i <= 5; i++)
+   for (int i = 0; i <= a; i++)
    {
     Console.WriteLine(i);
    }
   }
-  static void CountDown()
+  static void CountDown(int b)
   {
-   for (int i = 5; i >= 0; i--)
+   for (int i = b; i >= 0; i--)
    {
     Console.WriteLine(i);
    }
