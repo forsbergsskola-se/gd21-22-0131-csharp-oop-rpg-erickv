a1df39d1aaa365f3afda3d2312c2f3bbe29dc447
Erick Vaghi <vaghi.erick@gmail.com>
2022-02-07 15:47:27 +0100

    P2Classes Erick Completed

---
+++ projects/P2ClassesErick/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Classes
+{
+    class Person
+    {
+        static Person person = new Person();
+
+        static void Main()
+        {
+            Console.WriteLine(person);
+            Console.WriteLine(Animal.animal);
+            Console.WriteLine(Car.car);
+        }
+    }
+
+    class Animal
+    {
+        public static Animal animal = new Animal();
+    }
+
+    class Car
+    {
+        public static Car car = new Car();
+    }
+}
