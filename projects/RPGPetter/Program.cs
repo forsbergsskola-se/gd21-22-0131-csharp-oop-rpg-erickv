aeca27bbfdd59c672e9f2c36e1c9889ef528f20b
Petter Söderberg <petter.soderberg@student.forsbergsskola.se>
2022-02-17 20:48:50 +0100

    RPG created

---
+++ projects/RPGPetter/Program.cs
@@ -0,0 +1,14 @@
+﻿static class SuperMath
+{
+    public static float Lerp (float from, float to, float t)
+    {
+        return from + (to - from) * t;
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(SuperMath.Lerp(0,100,0));
+    }
+}
