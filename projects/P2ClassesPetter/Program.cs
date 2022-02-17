c2e13749c747102f9d70f870cfd492a8ffb879af
Petter Söderberg <petter.soderberg@student.forsbergsskola.se>
2022-02-17 20:25:31 +0100

    P2ClassesPetter Finished

--- projects/P2ClassesPetter/Program.cs
+++ projects/P2ClassesPetter/Program.cs
@@ -1,7 +1,4 @@
-﻿class Person
-{
-            
-}
+﻿
 
 class Animal
 {
@@ -13,24 +10,64 @@ class Car
             
 }
 class Program
+{
+    class Person
     {
-        public static void Main()
+       public string name;
+   
+        public void IntroduceYourself()
         {
+            Console.WriteLine("Your name is " + name +".");
+       
+        }
+    }
+    
+    public static void Main()
+    {
 
-            Person chinese;
-            chinese = new Person();
+      /* Testing:
+       Person chinese;
+        chinese = new Person();
+        chinese.name = "Chang";
+        chinese.IntroduceYourself();
 
-            Animal dog;
-            dog = new Animal();
+        Person italian;
+        italian = new Person();
+        italian.name = "Giorginio";
+        italian.IntroduceYourself(); */
 
-            Car volvo;
-            volvo = new Car();
-            
-            Console.WriteLine(chinese);
-            Console.WriteLine(dog);
-            Console.WriteLine(volvo);
 
+        Person [] people;
+        people = new Person [3];
+        for (int i = 0; i <= people.Length - 1; i++)
+        {
+            people[i] = new Person();
+            Console.WriteLine("Write a name.");
+            people[i].name = Console.ReadLine();
+        }
+        
+        /*
+        people[0].name = "Anders";
+       people[1].name = "Peder";
+       people[2].name = "Ulf"; */
+
+        for (int i = 0; i <= people.Length - 1; i++)
+        {
+            Person currentPerson = people[i];
+            currentPerson.IntroduceYourself();
         }
 
-        
+
+        /*   Animal dog;
+           dog = new Animal();
+   
+           Car volvo;
+           volvo = new Car();
+           
+           Console.WriteLine(chinese);
+           Console.WriteLine(dog);
+           Console.WriteLine(volvo);
+           */
+
     }
+}
