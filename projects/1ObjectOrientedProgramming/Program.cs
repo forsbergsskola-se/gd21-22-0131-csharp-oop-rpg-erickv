66e867a9e92a9b7994b5eb7fa3fe2b44a0e837e1
Petter Söderberg <petter.soderberg@student.forsbergsskola.se>
2022-02-17 19:23:39 +0100

    1ObjectOrientedProgramming Petter finished

--- projects/1ObjectOrientedProgramming/Program.cs
+++ projects/1ObjectOrientedProgramming/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ObjectOrientedProgramming
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string[] names = new string[3];
+            /*
+             names[0] = 
+             names[1] = 
+            names[2] =
+            */
+            for (int i = 0; i < names.Length; i++)
+            {
+              Console.WriteLine("Give me a name.");
+              names[i] = Console.ReadLine();
+            }
+            for (int i = 0; i < names.Length; i++)
+            {
+                /* Alternative 1:
+                if (i == 0 || i == 1)
+                {
+                    Console.Write(names[i] + ", ");
+                }
+                else
+                {
+                    Console.Write(names[i] + ".");
+                }
+                */
+                
+                //Alternative 2:
+                /* if (i != 0)
+                {
+                    Console.Write(", ");
+                }
+                Console.Write(names[i]); */
+                
+                //Alternative 3
+                Console.Write(names[i]);
+               if(i < names.Length-1)
+               {
+                   Console.Write(", ");
+               }
+                
+            }
+        }
+    }
+}
