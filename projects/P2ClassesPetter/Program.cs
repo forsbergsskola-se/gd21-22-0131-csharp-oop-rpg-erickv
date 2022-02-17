4f639be3b5da3aa5ff1f9c08bafa0d98fe943152
Petter Söderberg <petter.soderberg@student.forsbergsskola.se>
2022-02-17 19:38:04 +0100

    P2ClassesPetter Created and Finished

---
+++ projects/P2ClassesPetter/Program.cs
@@ -0,0 +1,36 @@
+﻿class Person
+{
+            
+}
+
+class Animal
+{
+            
+}
+
+class Car
+{
+            
+}
+class Program
+    {
+        public static void Main()
+        {
+
+            Person chinese;
+            chinese = new Person();
+
+            Animal dog;
+            dog = new Animal();
+
+            Car volvo;
+            volvo = new Car();
+            
+            Console.WriteLine(chinese);
+            Console.WriteLine(dog);
+            Console.WriteLine(volvo);
+
+        }
+
+        
+    }
