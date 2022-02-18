d6c02ff6909fdbc3361c288a551cb7316c4615f2
Erick Vaghi <vaghi.erick@gmail.com>
2022-02-18 20:21:32 +0100

    9 - Access Modifiers completed

--- projects/RPGErick/Program.cs
+++ projects/RPGErick/Program.cs
@@ -3,17 +3,28 @@
     public string name;
     public int id;
     public static int nextId;
+    private int maxHealth;
+    private int health;
 
-    public Unit(string name)
+    public Unit(string name, int maxHealth)
     {
         this.name = name;
         id = nextId++;
+
+        this.maxHealth = maxHealth;
+        health = maxHealth;
         ReportStatus();
     }
 
     public void ReportStatus()
     {
-        Console.WriteLine($"Unit #{id}: {name}");
+        Console.WriteLine($"\nUnit #{id}: {name} | {health}/{maxHealth} Hp\n");
+    }
+
+    public void SetHealth(int newHealth)
+    {
+        health = Math.Clamp(newHealth, 0, maxHealth);
+        ReportStatus();
     }
 }
 
@@ -21,8 +32,12 @@ static class Program
 {
     static void Main()
     {
-        Unit zombie = new Unit("Warrior");
-        Unit Wolf = new Unit("Wolf");
-        Unit Wizard = new Unit("Wizard");
+        Unit leet = new Unit("Leet", 1337);
+
+        for (int i = 0; i <= 2; i++)
+        {
+            Console.WriteLine("What do you want Leet's Health to be?");
+            leet.SetHealth(int.Parse(Console.ReadLine()));
+        }
     }
 }
